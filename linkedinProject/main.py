153bb823b41202d44b33a6c101a8c77b7ab14ad8
Zdravko Milinkovic <zdravkolegenda123@gmail.com>
2024-01-17 20:11:29 +0100

    Imeplementing keras model for learning neural network

--- linkedinProject/main.py
+++ linkedinProject/main.py
@@ -2,6 +2,12 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import tensorflow as tf
+from keras.preprocessing.text import one_hot
+from keras.utils import pad_sequences
+from tensorflow import keras
+from tensorflow.keras import layers
 from data import *
 
 def print_hi(name):
@@ -10,13 +16,71 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+if _name_ == '_main_':
 
     train, test = split_data(import_data())
-    #print(train)
-    #print(test)
     x_train, y_train, x_test, y_test = split_input_output(train, test)
 
-    print_hi('PyCharm')
+    num_locations = 5  # Number of unique issue tags
+    num_words = 10000  # Size of vocabulary obtained when preprocessing text data
+    num_pop_levels = 4  # Number of departments for predictions
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    title_input = keras.Input(
+        shape=(None,), name="title"
+    )  # Variable-length sequence of ints
+    description_input = keras.Input(shape=(None,), name="description")  # Variable-length sequence of ints
+    location_input = keras.Input(
+        shape=(num_locations,), name="location"
+    )  # Binary vectors of size num_tags
+
+    # Embed each word in the title into a 64-dimensional vector
+    title_features = layers.Embedding(num_words, 64)(title_input)
+    # Embed each word in the text into a 64-dimensional vector
+    description_features = layers.Embedding(num_words, 64)(description_input)
+
+    # Reduce sequence of embedded words in the title into a single 128-dimensional vector
+    title_features = layers.LSTM(128)(title_features)
+    # Reduce sequence of embedded words in the body into a single 32-dimensional vector
+    description_features = layers.LSTM(32)(description_features)
+
+    # Merge all available features into a single large vector via concatenation
+    x = layers.concatenate([title_features, description_features, location_input])
+
+    # Stick a department classifier on top of the features
+    popularity_pred = layers.Dense(1, name="views")(x)
+
+    # Instantiate an end-to-end model predicting both priority and department
+    model = keras.Model(
+        inputs=[title_input, description_input, location_input],
+        outputs=[popularity_pred],
+    )
+
+    model.compile(
+        optimizer=keras.optimizers.RMSprop(1e-3),
+        loss=[
+            keras.losses.BinaryCrossentropy(from_logits=True),
+            keras.losses.CategoricalCrossentropy(from_logits=True),
+        ],
+        loss_weights=[1.0, 0.2],
+        metrics=[keras.metrics.SparseCategoricalAccuracy(), "accuracy"],
+    )
+
+    # Dummy input data
+    encoded_titles = [one_hot(d, num_words) for d in x_train['title']]
+    padded_titles = pad_sequences(encoded_titles, maxlen=6, padding='post')
+    encoded_description = [one_hot(d, num_words) for d in x_train['description'].astype(str)]
+    padded_description = pad_sequences(encoded_description, maxlen=6, padding='post')
+    encoded_location = [one_hot(d, num_words) for d in x_train['location']]
+    padded_locatons = pad_sequences(encoded_location, maxlen=5, padding='post')
+    title_data = padded_titles
+    description_data = padded_description
+    location_data = padded_locatons
+    # Dummy target data
+    dept_targets = y_train
+
+    model.fit(
+        {"title": title_data, "description": description_data, "location": location_data},
+        {"views": dept_targets},
+        epochs=5,
+        batch_size=32,
+    )
